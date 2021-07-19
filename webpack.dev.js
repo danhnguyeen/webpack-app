36f4ed28dd7314da773286a44dd94ff9a6e0c88e
Danh Nguyen <danhcn09a@gmail.com>
2021-07-19 18:08:37 +0700

    Extract css

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const common = require("./webpack.common");
-const { merge } = require('webpack-merge');
+const { merge } = require("webpack-merge");
 const path = require("path");
 
 module.exports = merge(common, {
@@ -7,5 +7,20 @@ module.exports = merge(common, {
   output: {
     filename: "[name].bundle.js", // the name of the file after building
     path: path.resolve(__dirname, "build"), //define the path of building folder, default is dist
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
   },
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production", // by default, the mode is production
@@ -10,6 +11,22 @@ module.exports = merge(common, {
     filename: "[name].[contenthash].bundle.js", // the name of the file after building
     path: path.resolve(__dirname, "build"), //define the path of building folder, default is dist
     clean: true,
-    assetModuleFilename: "./imgs/[name].[hash].[ext]", // define the path and name of assets files
+    assetModuleFilename: "./imgs/[name].[contenthash].[ext]", // define the path and name of assets files
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: '[name].[contenthash].css'})],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extract css into files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
   },
 });
