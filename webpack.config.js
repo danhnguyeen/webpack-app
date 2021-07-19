1480ded5dfa20451ae6e2501bdf6a82791e4f6e3
Danh Nguyen <danhcn09a@gmail.com>
2021-07-19 15:10:53 +0700

    Cache and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,9 +6,12 @@ module.exports = {
   // devtool: false,
   entry: "./src/index.js", // the input file path that webpack will run in the first time
   output: {
-    filename: "main.js", // the name of the file after building
+    filename: "main.[contenthash].js", // the name of the file after building
     path: path.resolve(__dirname, 'build') //define the path of building folder, default is dist
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: './index.html'
+  })],
   module: {
     rules: [
       {
